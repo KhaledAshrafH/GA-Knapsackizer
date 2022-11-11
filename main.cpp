b4b0a0841272b84e40e75baef703f0bee87d2b0f
Khaled Ashraf <83093446+KhaledAshrafH@users.noreply.github.com>
2022-11-12 00:08:28 +0200

    New Commit

--- main.cpp
+++ main.cpp
@@ -1,44 +1,30 @@
 #include <bits/stdc++.h>
-#include <ctime>
-#include <cstdlib>
-#include <utility>
 #define endl '\n'
 #define ll long long
 using namespace std;
 
-const int populationSize = 1000, numOfIterations = 15;
-const double Pc = 0.8, Pm = 0.02;
+const int populationSize = 1374, numOfIterations = 74;
+const double Pc = 0.5, Pm = 0.05;
 int cnt=0;
 
 class Chromosome {
 private:
-    ll capacity=0;
-    ll numOfItems{};
-    double probability;
-    ll sumOfValues=0,sumOfWeights=0;
-    int itemNum;
+    ll capacity=0,numOfItems{},sumOfValues=0,sumOfWeights=0;;
+    double probability{};
+    int itemNum{};
 public:
     vector<bool> genes;
     Chromosome() {
         itemNum=++cnt;
     }
-
     virtual ~Chromosome() = default;
-
-    Chromosome(long long int numOfItems, const vector<bool> &genes, long long int sumOfValues,
-               long long int sumOfWeights, long long int capacity) : numOfItems(numOfItems), genes(genes), sumOfValues(sumOfValues),
-                                                                     sumOfWeights(sumOfWeights), capacity(capacity) {}
-
     void setItems(ll n,ll c) {
         this->capacity=c;
         this->numOfItems=n;
         genes.resize(this->numOfItems);
         for(int i=0;i<this->numOfItems;i++)
             genes[i]=false;
-
     }
-
-
     void calcFitness(const vector<pair<ll,ll>>& items){
         sumOfValues=0,sumOfWeights=0;
         for(auto i=0;i<items.size();i++){
@@ -47,40 +33,30 @@ public:
                 sumOfValues+=items[i].second;
             }
         }
-        if(sumOfWeights>capacity) {
-            sumOfValues=-1;
-        }
+        // handle the infeasible solution
+        if(sumOfWeights>capacity) sumOfValues=-1;
     }
 
-    long long int getSumOfValues() const {
+    [[nodiscard]] ll getSumOfValues() const {
         return sumOfValues;
     }
-
-    long long int getSumOfWeights() const {
+    [[nodiscard]] ll getSumOfWeights() const {
         return sumOfWeights;
     }
-
-    double getProbability() const {
+    [[nodiscard]] double getProbability() const {
         return probability;
     }
-
     void setProbability(double probability) {
         Chromosome::probability = probability;
     }
 
-    int getItemNum() const {
-        return itemNum;
-    }
-
-    void setItemNum(int itemNum) {
-        Chromosome::itemNum = itemNum;
-    }
 };
 
+//sort by probability
 bool compareProb(const Chromosome& c1,const Chromosome& c2){
     return c1.getProbability()<c2.getProbability();
 }
-
+//sort by values
 bool compareVal(const Chromosome& c1,const Chromosome& c2){
     return c1.getSumOfValues()>c2.getSumOfValues();
 }
@@ -89,27 +65,23 @@ class GA{
 private:
     vector<Chromosome>* solutions;
 public:
-    GA(vector<Chromosome>& solutions) {
+    explicit GA(vector<Chromosome>& solutions) {
         this->solutions=&solutions;
     }
-
     void initializePopulation(){
         for(auto & solution : *solutions){
             for(auto && gene : solution.genes){
-                gene=rand()%2;
+                gene=rand()&1;
             }
         }
     }
-
     void rouletteWheel(){
         auto & sol=*solutions;
-
         vector<Chromosome> res(sol.size());
         map<double,int> probabilityToIndex;
         double sumFitness=0.0,TEMP;
-        for(auto & solution : *solutions){
+        for(auto & solution : *solutions)
             sumFitness+=solution.getSumOfValues();
-        }
 
         TEMP=sol[0].getSumOfValues()/sumFitness;
         sol[0].setProbability(TEMP);
@@ -119,18 +91,18 @@ public:
             sol[i].setProbability(TEMP);
             probabilityToIndex[TEMP]=i;
         }
+        sort(sol.begin(),sol.end(), compareVal);
+        res[0]=sol[sol.size()-1];
+        res[1]=sol[sol.size()-2];
         sort(sol.begin(),sol.end(), compareProb);
-        for (int i = 0; i < populationSize; i++) {
+        for (int i = 2; i < populationSize; i++) {
             double random_key = ((double) rand() / (RAND_MAX));
-            auto iter = probabilityToIndex.upper_bound(random_key-0.1);
-            if (iter != probabilityToIndex.begin())
-                iter--;
+            auto iter = probabilityToIndex.upper_bound(random_key-0.2);
+            if (iter != probabilityToIndex.begin()) iter--;
             res[i]=sol[iter->second];
         }
-        for (auto i=0;i<populationSize;i++) {
+        for (auto i=0;i<populationSize;i++)
             sol[i]=res[i];
-        }
-
         sort(sol.begin(),sol.end(),compareVal);
     }
     void crossOver(){
@@ -139,10 +111,9 @@ public:
             for(int j=i+1;j<populationSize;j++){
                 double randNum=((double) rand() / (RAND_MAX));
                 if(randNum<Pc){
-                    int randNumIdx=rand()%(sol[i].genes.size()-1);
-                    for(int k=randNumIdx;k<sol[i].genes.size();k++){
+                    int randNumIdx=rand()%(sol[i].genes.size()-2);
+                    for(int k=randNumIdx;k<sol[i].genes.size();k++)
                         swap(sol[i].genes[k],sol[j].genes[k]);
-                    }
                 }
             }
         }
@@ -152,28 +123,26 @@ public:
         for(int i=0;i<populationSize;i++){
             for(auto && gene : sol[i].genes){
                 double randNum=((double) rand() / (RAND_MAX));
-                if(randNum<Pm)
-                    gene=!gene;
+                if(randNum<Pm) gene=!gene;
             }
         }
     }
-
 };
-
 
 int main() {
     srand(time(nullptr));
-
-    ll numOftestCases,numOfItems,capacity,iterationsSize;cin>>numOftestCases;
+    freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    ll numOftestCases,numOfItems,capacity,iterationsSize,testNum=1,numOfSelected;cin>>numOftestCases;
     while(numOftestCases--){
-        iterationsSize=numOfIterations;
+        numOfSelected=0;
+        iterationsSize=numOftestCases>-1?numOfIterations:100;
         cin>>capacity>>numOfItems;
         vector<pair<ll,ll>> items(numOfItems);
         for(auto i=0;i<numOfItems;i++) cin>>items[i].first>>items[i].second;
         vector<Chromosome> solutions(populationSize);
         for (int i = 0; i < populationSize; i++)
             solutions[i].setItems(numOfItems,capacity);
-
         GA geneticAlgorithm(solutions);
 
         //First Step (Initialize Pool Of Solutions)
@@ -183,7 +152,6 @@ int main() {
             //Second Step (Individual Evaluation)
             for (int i = 0; i < populationSize; i++)
                 solutions[i].calcFitness(items);
-
             sort(solutions.begin(),solutions.end(),compareVal);
             //Third Step (Selection)
             geneticAlgorithm.rouletteWheel();
@@ -191,13 +159,22 @@ int main() {
             //Fourth Step (Crossover)
             geneticAlgorithm.crossOver();
 
-            //Fourth Step (Mutation)
+            //Fifth Step (Mutation)
             geneticAlgorithm.mutation();
 
-            //Fifth Step (Reproduction)
+            //Sixth Step (Reproduction)
         }
         sort(solutions.begin(),solutions.end(),compareVal);
-        cout<<solutions[0].getSumOfValues()<<endl;
+        for(auto && gene : solutions[0].genes){
+            if(gene) numOfSelected++;
+        }
+        cout<<"Testcase "<<testNum++<<":\n"<<"Number of selected items: "<<numOfSelected<<endl
+        <<"Total Value: "<<solutions[0].getSumOfValues()<<endl;
+        for(int i=0;i<solutions[0].genes.size();i++){
+            if(solutions[i].genes[i])
+                cout<<"Item "<<i+1<<": "<<"Weight = "<<items[i].first<<", Value = "<<items[i].second<<endl;
+        }
+        cout<<endl;
     }
     return 0;
 }
